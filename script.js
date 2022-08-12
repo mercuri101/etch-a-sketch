c28fa32aab23e7c09dca40dc609d7eb9a579a765
mercuri <mercuri.abb@gmail.com>
2022-08-12 21:03:27 +0600

    Implement hover-coloring of cells

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const body = document.querySelector("body");
 const field = document.querySelector(".field");
 
+
+// Create 16x16 grid
 for (row = 0; row < 16; row++) {
   let newRow = document.createElement("div");
   newRow.classList.add("row");
@@ -10,6 +12,15 @@ for (row = 0; row < 16; row++) {
     newCell.classList.add("cell");
     newRow.appendChild(newCell);
   }
-
   field.appendChild(newRow);
+}
+
+// Implement hover-coloring
+const color = "blue";
+const cells = document.querySelectorAll(".cell");
+cells.forEach(cell => cell.addEventListener("mouseover", () => colorCell(cell, color)));
+
+
+function colorCell(cell, color) {
+  cell.style.backgroundColor = color;
 }

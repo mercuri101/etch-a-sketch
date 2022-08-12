111f95515f812538f4517c68ab71f9266965d8ff
mercuri <mercuri.abb@gmail.com>
2022-08-12 21:50:46 +0600

    Setup a button to create new grid

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
 const body = document.querySelector("body");
 const field = document.querySelector(".field");
+const newGrindButton = document.querySelector("button");
 
+const curColor = "blue";
 
-// Create 16x16 grid
-for (row = 0; row < 16; row++) {
-  let newRow = document.createElement("div");
-  newRow.classList.add("row");
-
-  for (col = 0; col < 16; col++) {
-    let newCell = document.createElement("div");
-    newCell.classList.add("cell");
-    newRow.appendChild(newCell);
-  }
-  field.appendChild(newRow);
-}
+createGrid(16);
+newGrindButton.addEventListener("click", () => createNewGrid());
 
 // Implement hover-coloring
-const color = "blue";
-const cells = document.querySelectorAll(".cell");
-cells.forEach(cell => cell.addEventListener("mouseover", () => colorCell(cell, color)));
+
 
 
 function colorCell(cell, color) {
   cell.style.backgroundColor = color;
+}
+
+
+function createGrid(size) {
+  for (row = 0; row < size; row++) {
+    let newRow = document.createElement("div");
+    newRow.classList.add("row");
+
+    for (col = 0; col < size; col++) {
+      let newCell = document.createElement("div");
+      newCell.classList.add("cell");
+      newRow.appendChild(newCell);
+    }
+    field.appendChild(newRow);
+  }
+
+  addHoverColoring(curColor);
+}
+
+
+function createNewGrid() {
+  let size;
+
+  // Keep prompting for a size between 0 and 101
+  do {
+    size = prompt("Enter size of the new grid (up to 100):");
+  } while (size <= 0 || size > 100);
+
+  let rowsToDelete = document.querySelectorAll(".row");
+  rowsToDelete.forEach(row => row.remove());
+
+  createGrid(size);
+}
+
+
+function addHoverColoring(color) {
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach(cell => cell.addEventListener("mouseover", () => colorCell(cell, color)));
 }

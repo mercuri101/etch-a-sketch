2ade335a543982ef085062e382ef2e3963f7708e
mercuri <mercuri.abb@gmail.com>
2022-08-15 18:59:26 +0600

    Implement drawing and erasing modes

--- script.js
+++ script.js
@@ -2,19 +2,20 @@ const body = document.querySelector("body");
 const field = document.querySelector(".field");
 const newGrindButton = document.querySelector(".new-grid-btn");
 const colors = document.querySelectorAll(".color");
+const drawerBtn = document.querySelector(".drawer");
+const eraserBtn = document.querySelector(".eraser");
 
-const curColor = "blue";
+let curColor = "blue";
+let emptyCellBorder = "1px solid gray";
+let emptyCellColor = "white";
 
 createGrid(16);
 newGrindButton.addEventListener("click", () => createNewGrid());
 
 setupColorPalette(colors);
+drawerBtn.addEventListener("click", () => setDrawingMode(curColor));
+eraserBtn.addEventListener("click", () => setErasingMode());
 
-
-function colorCell(cell, color) {
-  cell.style.backgroundColor = color;
-  cell.style.border = `1px solid ${color}`;
-}
 
 
 function createGrid(size) {
@@ -25,12 +26,14 @@ function createGrid(size) {
     for (col = 0; col < size; col++) {
       let newCell = document.createElement("div");
       newCell.classList.add("cell");
+      newCell.style.border = emptyCellBorder;
+      newCell.style.backgroundColor = emptyCellColor;
       newRow.appendChild(newCell);
     }
     field.appendChild(newRow);
   }
 
-  addHoverColoring(curColor);
+  setDrawingMode(curColor);
 }
 
 
@@ -49,7 +52,12 @@ function createNewGrid() {
 }
 
 
-function addHoverColoring(color) {
+function setDrawingMode(color) {
+  drawerBtn.classList.add("active");
+  drawerBtn.classList.remove("inactive-hover");
+  eraserBtn.classList.remove("active");
+  eraserBtn.classList.add("inactive-hover");
+
   let mouseIsDown = false;
 
   field.addEventListener("mousedown", () => mouseIsDown = true);
@@ -61,6 +69,34 @@ function addHoverColoring(color) {
 }
 
 
+function setErasingMode() {
+  eraserBtn.classList.add("active");
+  eraserBtn.classList.remove("inactive-hover");
+  drawerBtn.classList.remove("active");
+  drawerBtn.classList.add("inactive-hover");
+
+  let mouseIsDown = false;
+
+  field.addEventListener("mousedown", () => mouseIsDown = true);
+  field.addEventListener("mouseup", () => mouseIsDown = false);
+
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach(cell => cell.addEventListener("mouseover", () => mouseIsDown ? eraseCell(cell) : undefined));
+  cells.forEach(cell => cell.addEventListener("mousedown", () => eraseCell(cell)));
+}
+
+
 function setupColorPalette(colors) {
   colors.forEach(color => color.style.backgroundColor = color.getAttribute("data-color"));
+}
+
+
+function colorCell(cell, color) {
+  cell.style.backgroundColor = color;
+  cell.style.border = `1px solid ${color}`;
+}
+
+function eraseCell(cell) {
+  cell.style.backgroundColor = emptyCellColor;
+  cell.style.border = emptyCellBorder;
 }
